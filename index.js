ada9a92a25e765e51279358125f4a59e336a0dc2
Monarch Wadia <monarch@mintbean.io>
2021-05-18 22:31:03 -0400

    now creating schema from file

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const fs = require("fs");
+const path = require("path");
+
+const schemaFile = fs.readFileSync(
+  path.resolve(__dirname, "schema.graphql"),
+  "utf-8"
+);
 
 const app = express();
 
-const schema = buildSchema(`
-  type Book {
-    title: String!
-    author: String!
-  }
-
-  type Query {
-    books: [Book]
-  }
-`);
+const schema = buildSchema(schemaFile);
 
 const rootValue = {
   books: [
