e458698c7e7c8a25f25b5ee0496468b6008a6173
Monarch Wadia <monarch@mintbean.io>
2021-05-23 15:30:45 -0400

    Now caching knex as a singleton.

--- src/data/knex-provider.ts
+++ src/data/knex-provider.ts
@@ -1,33 +1,37 @@
-import Knex from "knex";
+import Knex, { Knex as IKnex } from "knex";
 import fs from "fs";
 import path from "path";
 
+let instance: IKnex<any, unknown[]>;
+
 const getKnex = async () => {
-  const { MB_KNEXFILE } = process.env;
+  if (!instance) {
+    const { MB_KNEXFILE } = process.env;
 
-  if (!MB_KNEXFILE) {
-    throw new Error("Env variable MB_KNEXFILE must be provided.");
+    if (!MB_KNEXFILE) {
+      throw new Error("Env variable MB_KNEXFILE must be provided.");
+    }
+
+    const knexfilePath = path.resolve(__dirname, "../..", MB_KNEXFILE);
+
+    const stat = fs.statSync(knexfilePath);
+    if (!stat.isFile) {
+      throw new Error(`Knexfile at ${MB_KNEXFILE} is not a file.`);
+    }
+
+    let knexfile;
+    try {
+      // const knexFileString = fs.readFileSync(MB_KNEXFILE, "utf-8");
+      knexfile = await import(knexfilePath);
+    } catch (e) {
+      console.error(`Failed to load knexfile at ${MB_KNEXFILE}!`);
+      throw e;
+    }
+
+    instance = Knex(knexfile);
   }
 
-  const knexfilePath = path.join(__dirname, "../..", MB_KNEXFILE);
-
-  const stat = fs.statSync(knexfilePath);
-  if (!stat.isFile) {
-    throw new Error(`Knexfile at ${MB_KNEXFILE} is not a file.`);
-  }
-
-  let knexfile;
-  try {
-    // const knexFileString = fs.readFileSync(MB_KNEXFILE, "utf-8");
-    knexfile = await import(knexfilePath);
-  } catch (e) {
-    console.error(`Failed to load knexfile at ${MB_KNEXFILE}!`);
-    throw e;
-  }
-
-  const knex = Knex(knexfile);
-  console.log("knex.raw", knex.raw);
-  return knex;
+  return instance;
 };
 
 export default getKnex;
