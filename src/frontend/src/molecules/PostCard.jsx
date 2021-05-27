62471f1e14afa8791b879a045f0b34cd48c8181e
Monarch Wadia <monarch@mintbean.io>
2021-05-27 13:51:47 -0400

    Added like section to Postcard UI

--- src/frontend/src/molecules/PostCard.jsx
+++ src/frontend/src/molecules/PostCard.jsx
@@ -7,21 +7,28 @@ const PostCard = ({ post }) => {
   const { id: communityId, title: communityTitle } = community;
 
   return (
-    <div className={$.postCard}>
-      <div className={$.header}>{title}</div>
-      <div className={$.body}>
-        <div className={$.preview}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quo, excepturi animi exercitationem eaque cum
-          impedit voluptates nemo praesentium dicta veniam. A reiciendis veniam provident sint porro. Quod, non illo?
-        </div>
-        <div className={$.summary}>
-          <div>
-            {firstName} {lastName}
+    <div className={$.root}>
+      <div className={$.like}>
+        <div className={$.heart}>&lt;3</div>
+        <div className={$.likeCount}>29</div>
+      </div>
+      <div className={$.main}>
+        <div className={$.header}>{title}</div>
+        <div className={$.body}>
+          <div className={$.preview}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quo, excepturi animi exercitationem eaque
+            cum impedit voluptates nemo praesentium dicta veniam. A reiciendis veniam provident sint porro. Quod, non
+            illo?
           </div>
-          <div>·</div>
-          <div>{communityTitle}</div>
-          <div>·</div>
-          <div>36 Comments</div>
+          <div className={$.summary}>
+            <div>
+              {firstName} {lastName}
+            </div>
+            <div>·</div>
+            <div>{communityTitle}</div>
+            <div>·</div>
+            <div>36 Comments</div>
+          </div>
         </div>
       </div>
     </div>
