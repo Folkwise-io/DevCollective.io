49b198f6013b910131e6ed908fb012e4e3c45d6d
Monarch Wadia <monarch@mintbean.io>
2021-06-02 22:38:29 -0400

    Added color palette and modified  colors.

--- src/frontend/src/elements/BorderBox.jsx
+++ src/frontend/src/elements/BorderBox.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const BorderBox = styled.div`
-  border: 2px solid var(--main-color);
+  border: 2px solid var(--blue-700);
   padding: 1rem;
 `;
 
--- src/frontend/src/organisms/NavBar.jsx
+++ src/frontend/src/organisms/NavBar.jsx
@@ -9,24 +9,28 @@ const Container = styled.div`
   grid-auto-flow: column;
   justify-content: space-between;
   align-items: center;
-  background: var(--main-color);
+  background: var(--space-900);
   padding: 1rem 2rem;
+  border-bottom: 1px solid var(--space-500);
 `;
 
 const Logo = styled.span`
   font-size: 2em;
-  color: var(--white);
+  color: var(--brand-400);
   font-weight: 600;
+  text-decoration: none;
+  cursor: pointer;
+  user-select: none;
 `;
 
 const NavBar = () => {
   return (
     <Container>
-      <Link to="/">
-        <Logo>
-          <a>DevCollective.io</a>
-        </Logo>
-      </Link>
+      <Logo>
+        <Link to="/" style={{ textDecoration: "inherit", color: "inherit" }}>
+          DevCollective.io
+        </Link>
+      </Logo>
       <AuthButton />
     </Container>
   );
