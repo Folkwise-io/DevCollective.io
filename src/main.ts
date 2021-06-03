0ad7d3b39c7bbbf6a779cd4c2285db0993488715
Monarch Wadia <monarch@mintbean.io>
2021-06-02 20:35:36 -0400

    Improvements to dev process - auto open browser on frontend launch; rename vscode launch configs and make Server the default one.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import appFactory from "./appFactory";
 import frontendDevMiddleware from "./frontend/frontendDevMiddleware";
+import open from "open";
+import { AddressInfo } from "net";
+import configProvider from "./configProvider";
+const { MB_AUTO_OPEN_BROWSER } = configProvider();
 
 const app = appFactory();
 frontendDevMiddleware(app);
-app.listen(8080, () => console.log("Started on port 8080"));
+const server = app.listen(8080, () => {
+  const port = (server.address() as AddressInfo)?.port;
+  console.log(`Started on port ${port}`);
+  if (MB_AUTO_OPEN_BROWSER) {
+    open(`http://localhost:${port}`);
+  }
+});
