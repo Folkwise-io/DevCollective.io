01f573cb711e0cd86cdba6cac446cfd5e354dfe0
Brandon Tsang <brandononline2@gmail.com>
2021-06-30 12:56:30 -0400

    add edit user form

--- src/frontend/src/pages/user/UserPage.jsx
+++ src/frontend/src/pages/user/UserPage.jsx
@@ -1,8 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router";
 
 const UserPage = () => {
   const { userId } = useParams();
+
+  const { register, handleSubmit } = useForm();
 
   const { loading, error, data } = useQuery(
     gql`
@@ -20,12 +23,40 @@ const UserPage = () => {
     },
   );
 
+  const [submitEditUser] = useMutation(gql`
+    mutation EditUserMutation($id: Int!, $firstName: String!, $lastName: String!) {
+      editUser(id: $id, editUserInput: { firstName: $firstName, lastName: $lastName }) {
+        id
+        firstName
+        lastName
+      }
+    }
+  `);
+
+  const onSubmit = handleSubmit((data) => {
+    submitEditUser({
+      variables: {
+        id: parseInt(userId),
+        firstName: data.firstName,
+        lastName: data.lastName,
+      },
+    });
+  });
+
   if (loading) return null;
   if (error) return <p>Sorry, we couldn&apos;t find that user.</p>;
 
   return (
     <div>
       {data.user.firstName} {data.user.lastName}
+      <h2>Edit user</h2>
+      <form onSubmit={onSubmit}>
+        <label>First name</label>
+        <input {...register(`firstName`)} />
+        <label>Last name</label>
+        <input {...register(`lastName`)} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
